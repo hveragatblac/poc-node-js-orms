08e27d6aa85af7a6ad7aa5e86de4fb529aee900d
Hector Vera <hector.vera@gatblac.com>
2023-08-18 14:24:09 -0400

    Add Prisma and NestJS's Prisma recipe

---
+++ src/database/prisma/prisma.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { PrismaController } from './prisma.controller';
+
+@Module({
+  providers: [PrismaService, UserService, PostService],
+  controllers: [PrismaController]
+})
+export class PrismaModule {}
