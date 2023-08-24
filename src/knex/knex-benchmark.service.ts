85b8b6670a9cd31e730b1abb0b610619ae20445b
Hector Vera <hector.vera@gatblac.com>
2023-08-24 15:22:01 -0400

    Knex amalgamation generator

--- src/knex/knex-benchmark.service.ts
+++ src/knex/knex-benchmark.service.ts
@@ -24,70 +24,70 @@ export class KnexBenchmarkService implements Benchmarkable {
         },
         generateArguments: generateAmagamationKnex,
       },
-      // {
-      //   name: 'Bulk insert',
-      //   task: async (dtos) => {
-      //     await this.amalgamationService.saveMany(dtos);
-      //   },
-      //   generateArguments: () =>
-      //     Array.from({ length: 2000 }).map(generateAmalgamation),
-      // },
-      // {
-      //   name: 'First select',
-      //   task: async () => {},
-      //   generateArguments: () => undefined,
-      // },
-      // {
-      //   name: 'Unique select',
-      //   task: async () => {},
-      //   generateArguments: () => undefined,
-      // },
-      // {
-      //   name: 'select',
-      //   task: async () => {},
-      //   generateArguments: () => undefined,
-      // },
-      // {
-      //   name: 'Single update',
-      //   task: async ({ dto, criterion }) => {
-      //     await this.amalgamationService.updateFirst(dto, criterion);
-      //   },
-      //   generateArguments: () => {
-      //     const dto = generateAmalgamation();
-      //     const criterion = { name: dto.name };
-      //     return { dto, criterion };
-      //   },
-      // },
-      // {
-      //   name: 'Bulk update',
-      //   task: async ({ dto, criterion }) => {
-      //     await this.amalgamationService.updateMany(dto, criterion);
-      //   },
-      //   generateArguments: () => {
-      //     const dtos = Array.from({ length: 2000 });
-      //     const names = Array.from({ length: 2000 });
-      //     for (let i = 0; i < dtos.length; i++) {
-      //       dtos[i] = generateAmalgamation();
-      //       names[i] = (dtos[i] as any).name;
-      //     }
-      //     const criterion = { name: { IN: names } };
-      //     return { dtos, criterion };
-      //   },
-      // },
-      // {
-      //   name: 'First delete',
-      //   task: async (criterion) => {
-      //     await this.amalgamationService.deleteFirst(criterion);
-      //   },
-      //   generateArguments: () => undefined,
-      // },
-      // {
-      //   name: 'Bulk delete',
-      //   task: async (criterion) => {
-      //     await this.amalgamationService.deleteMany(criterion);
-      //   },
-      //   generateArguments: () => undefined,
-      // },
+      {
+        name: 'Bulk insert',
+        task: async (dtos) => {
+          await this.amalgamationService.saveMany(dtos);
+        },
+        generateArguments: () =>
+          Array.from({ length: 2000 }).map(generateAmagamationKnex),
+      },
+      {
+        name: 'First select',
+        task: async () => {},
+        generateArguments: () => undefined,
+      },
+      {
+        name: 'Unique select',
+        task: async () => {},
+        generateArguments: () => undefined,
+      },
+      {
+        name: 'select',
+        task: async () => {},
+        generateArguments: () => undefined,
+      },
+      {
+        name: 'Single update',
+        task: async ({ dto, criterion }) => {
+          await this.amalgamationService.updateFirst(dto, criterion);
+        },
+        generateArguments: () => {
+          const dto = generateAmagamationKnex();
+          const criterion = { name: dto.name };
+          return { dto, criterion };
+        },
+      },
+      {
+        name: 'Bulk update',
+        task: async ({ dto, criterion }) => {
+          await this.amalgamationService.updateMany(dto, criterion);
+        },
+        generateArguments: () => {
+          const dtos = Array.from({ length: 2000 });
+          const names = Array.from({ length: 2000 });
+          for (let i = 0; i < dtos.length; i++) {
+            dtos[i] = generateAmagamationKnex();
+            names[i] = (dtos[i] as any).name;
+          }
+          const criterion = { name: { IN: names } };
+          return { dtos, criterion };
+        },
+      },
+      {
+        name: 'First delete',
+        task: async (criterion) => {
+          await this.amalgamationService.deleteFirst(criterion);
+        },
+        generateArguments: () => undefined,
+      },
+      {
+        name: 'Bulk delete',
+        task: async (criterion) => {
+          await this.amalgamationService.deleteMany(criterion);
+        },
+        generateArguments: () => undefined,
+      },
     ];
   }
 }
